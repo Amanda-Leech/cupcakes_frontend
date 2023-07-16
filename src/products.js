8fab118c31f37958d96b8466b7d01ee84676cfdc
amanda <amanda@devpipeline.com>
2023-07-16 18:16:02 -0400

    Add products

---
+++ src/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        _id: "1",
+        name: "Pink ice cream cone cupcake",
+        image: "/img/strawberrypink.jpg",
+        description: "strawberry, chocolate, or vanilla cake with pink frosting. topped with chocolate and sprinkles.",
+        brand: "home made",
+        category: "cupcakes",
+        price: "20 a dozen",
+        countInStock: 4,
+        rating: 4.3,
+        numReviews: 4,
+    },
+    {
+        _id: "2",
+        name: "White ice cream cone cupcake",
+        image: "/img/strawberrywhite.jpg",
+        description: "strawberry, chocolate, or vanilla cake with white frosting. topped with chocolate and sprinkles.",
+        brand: "home made",
+        category: "cupcakes",
+        price: "20 a dozen",
+        countInStock: 4,
+        rating: 4.5,
+        numReviews: 4,
+    },
+];
+
+export default products;
